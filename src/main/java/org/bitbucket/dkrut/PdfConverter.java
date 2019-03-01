8d3783d9099ee872ead027cee08b30fdd9d23fad
master <dd.krutikov@gmail.com>
2019-03-01 21:45:47 +0300

    added converter PDF file to PNG

---
+++ src/main/java/org/bitbucket/dkrut/PdfConverter.java
@@ -0,0 +1,48 @@
+package org.bitbucket.dkrut;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by Denis Krutikov on 26.02.2019.
+ */
+
+public class PdfConverter {
+    public void pdfConvert() {
+
+        String sourceDir = "src/main/resources/test1.pdf";
+        String destinationDir = "src/main/resources/testFiles";
+        File sourceFile = new File(sourceDir);
+        File destinationFile = new File(destinationDir);
+
+        try {
+            if (!destinationFile.exists()) {
+                destinationFile.mkdir();
+            }
+            if (sourceFile.exists()) {
+                PDDocument document = PDDocument.load(sourceDir);
+                List<PDPage> list = document.getDocumentCatalog().getAllPages();
+
+                String fileName = sourceFile.getName().replace(".pdf", ""); //get file name without extension
+                int pageNumber = 1;
+                //convert pdf to png, adding index to all pages
+                for (PDPage page : list) {
+                    BufferedImage image = page.convertToImage();
+                    File outputfile = new File(destinationDir + "/" + fileName + "_" + pageNumber + ".png");
+                    ImageIO.write(image, "png", outputfile);
+                    pageNumber++;
+                }
+                document.close();
+            } else {
+                System.err.println(sourceFile.getName() +" - file not exists");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
