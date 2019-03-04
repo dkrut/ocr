9a83e92256ae9e3aa9021c35d46e77d6df24417e
master <dd.krutikov@gmail.com>
2019-03-05 00:05:16 +0300

    OCR each file in turn, not all at once, and without copying to a temp folder

--- src/main/java/org/bitbucket/dkrut/ImageText.java
+++ src/main/java/org/bitbucket/dkrut/ImageText.java
@@ -1,16 +1,12 @@
 package org.bitbucket.dkrut;
 
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 
 /**
  * Created by Denis Krutikov on 26.02.2019.
@@ -20,63 +16,48 @@ public class ImageText {
 
     public static void main(String[] args) {
 
-        Logger log = LoggerFactory.getLogger(ImageText.class);
+        Logger log = LoggerFactory.getLogger(asd.class);
 
-        Properties tesseractProperties = new Properties();
-        try {
-            FileInputStream fis = new FileInputStream("src/main/resources/tesseract.properties");
-            tesseractProperties.load(fis);
-        } catch (IOException e){
-            System.err.println("Error while reading Tesseract properties: " + e.getMessage());
-        }
+        Ocr ocr = new Ocr();
+        PdfConverter pdfConverter = new PdfConverter();
 
         File sourceDir = new File("src/main/resources/testFiles");
         File tempFolder = new File("src/main/resources/temp");
-
-        PdfConverter pdfConverter = new PdfConverter();
-
-        Tesseract instance = new Tesseract();
-        instance.setDatapath("src/main/resources/tessdata");
-        instance.setLanguage(tesseractProperties.getProperty("language"));
+        File outPutFolder = new File("src/main/resources/outPutResult");
 
         if (sourceDir.exists() && sourceDir.list() != null) {
-            log.info(sourceDir + "have files to OCR");
-            try {
-                if (!tempFolder.exists()) {
-                    tempFolder.mkdir();
-                    log.info("Temp folder created");
-                }
+            log.info(sourceDir + " have files to OCR");
 
+            if (!outPutFolder.exists()){
+                log.warn(outPutFolder + " doesn't exist. Trying to create");
+                outPutFolder.mkdir();
+                log.info(outPutFolder.getName() + " folder created");
+            }
+
+            try {
                 File[] sourceDirFiles = sourceDir.listFiles();
                 assert sourceDirFiles != null;
                 for (File checkingFile : sourceDirFiles) {
                     String fileNameWithExtension = checkingFile.toString();
+                    File ocrResultFile = new File("src/main/resources/outPutResult/" + FilenameUtils.removeExtension(checkingFile.getName()) + ".txt");
                     if (FilenameUtils.getExtension(fileNameWithExtension).equals("pdf")){
                         pdfConverter.pdfConvert(checkingFile);
-                    } else FileUtils.copyFileToDirectory(checkingFile, tempFolder);
+
+                        File[] tempFolderFiles = tempFolder.listFiles();
+                        assert tempFolderFiles != null;
+                        for (File ocrResult : tempFolderFiles) {
+                            ocr.ocrToFile(ocrResult, ocrResultFile);
+                        }
+                        FileUtils.forceDelete(tempFolder);
+                        log.info("Temp folder deleted");
+
+                    } else ocr.ocrToFile(checkingFile, ocrResultFile);
                 }
-
-                File[] outputTempFiles = tempFolder.listFiles();
-
-                assert outputTempFiles != null;
-                for (File ocrResult : outputTempFiles) {
-                    log.info("Start OCR " + ocrResult.getName());
-                    System.out.println(instance.doOCR(ocrResult));
-                }
-
-                FileUtils.forceDelete(tempFolder);
-                log.info("Temp folder deleted");
-
-
-            } catch (TesseractException e) {
-                log.error("Error while reading image: " + e.getMessage());
-                e.printStackTrace();
             } catch (IOException e) {
                 log.error("Error while coping/deleting dir: " + e.getMessage());
                 e.printStackTrace();
             }
         } else log.warn(sourceDir.getName() + " is empty or doesn't exist");
-        log.info("OCR finished");
+        log.info("All OCR finished");
     }
-
 }
